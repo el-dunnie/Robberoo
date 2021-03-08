e913fe11fe3f4fc659fd589283102473118d8e66
Stan de Groot <standegrootste@hotmail.com>
2021-03-08 11:57:33 +0100

    player movement basic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float m_MoveSpeed;
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void Update()
+    {
+        Vector2 dir = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position));
+        float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.right * m_MoveSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
